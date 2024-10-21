bdb3a6fb4aeeaff4eb4a454eb6a6c3622e5aa658
ha1238 <ha1238@postech.ac.kr>
2024-10-21 23:43:07 +0900

    #2-1 Solver : done, neighbors, from

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b.isStanding && b.b1 == goal
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,7 +28,12 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = {
+    val addMove = for {
+      (block, move) <- b.legalNeighbors
+    } yield (block, move :: history)
+    addMove.toStream
+  }
 
   /**
    * This function returns the list of neighbors without the block
@@ -36,7 +41,13 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] = {
+    val addMove = for {
+      (b, move) <- neighbors
+      if !explored.contains(b)
+    } yield (b, move)
+    addMove.toStream
+  }
 
   /**
    * The function `from` returns the stream of all possible paths
@@ -62,7 +73,17 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = {
+    def traversal(queue : Stream[(Block, List[Move])],
+                  expl : Set[Block]): Stream[(Block, List[Move])]) = queue match{
+      case Stream.Empty => Stream.empty
+      case (b, hist) #:: tail =>
+        val newQueue = tail #::: newNeighborsOnly(neighborsWithHistory(b, hist), expl)
+        val newExpl = expl ++ newQueue.map { case (b,_) => b }
+        (b,hist) #:: traversal(newQueue, newExpl)
+    }
+    traversal(initial, explored)
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
